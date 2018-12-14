a5fcadb93ed3b478fd4147104da05001f458fa4c
Samy <samy@zoicstudios.com>
2018-12-14 10:15:38 -0800

    prep for pull

--- DSA/main.swift
+++ DSA/main.swift
@@ -1,11 +1,1 @@
-//
-//  main.swift
-//  DSA
-//
-//  Created by Samy on 12/13/18.
-//  Copyright © 2018 Samy. All rights reserved.
-//
 
-let test = SolutionMerge()
-
-print(test.mergeSort([3,1,6,8,3,2]))
